6d7cb97a9e5fc4cce7680a2db3cea880a4c8fbca
Olivia Asch <olivia.c.asch@gmail.com>
2022-01-19 19:32:27 +0000

    Only show settings when a file is provided

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,23 +1,27 @@
+import { useContext } from 'react';
 import { ImageSelectionForm } from '../components/ImageSelectionForm';
 import { ResultCanvas } from '../components/ResultCanvas';
 import { SettingsPanel } from '../components/SettingsPanel';
+import { FileContext } from '../context';
 
-export const Homepage = () => (
-  <div className="container-md my-4">
-    <div className="row mb-4">
-      <div className="col-12">
-        <ImageSelectionForm />
+export const Homepage = () => {
+  const { file } = useContext(FileContext);
+
+  return (
+    <div className="container-md my-4">
+      <div className="row mb-4">
+        <div className="col-12">
+          <ImageSelectionForm />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col d-flex flex-column align-items-center">
+          <ResultCanvas />
+        </div>
+
+        <div className="col-md-4">{!!file && <SettingsPanel />}</div>
       </div>
     </div>
-
-    <div className="row">
-      <div className="col d-flex flex-column align-items-center">
-        <ResultCanvas />
-      </div>
-
-      <div className="col-md-4">
-        <SettingsPanel />
-      </div>
-    </div>
-  </div>
-);
+  );
+};
